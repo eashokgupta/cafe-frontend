b140899716469226968836a23dc9f947fe1304cc
Ashok Gupta <ashok.gupta@etelligens.in>
2024-01-26 10:43:35 +0530

    cafe management project sturcture created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
